69386471d39749246e9a09fc071a17525df2e69f
WaiWlyan <hlyanw@yahoo.com>
2023-01-01 21:19:42 +0630

    villains and hero

--- jan_1_villin.py
+++ jan_1_villin.py
@@ -1,4 +1,4 @@
 avengers = ['Dr.Strange', 'Spiderman', 'Ironman']
 villains = ['v1', 'v2', 'v3']
 for h in range(len(villains)):
-    print(f"Hero {avengers[h]} fight with {villains[h]}")
+    print(f"hero {avengers[h]} fight with {villains[h]}")
