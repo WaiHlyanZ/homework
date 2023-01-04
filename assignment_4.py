9c2eeb5156ae083a2eacda1add2776fa3e04a78e
Ryuma <114047336+WaiWlyan3@users.noreply.github.com>
2023-01-04 22:28:44 +0630

    Update_jan_4

--- assignment_4.py
+++ assignment_4.py
@@ -1,3 +1,23 @@
+#assignment1
+name=input('What is your name?')
+num=int(input('Put a number:'))
+if num<10:
+    for item in range(num):
+        print(name)
+else:
+    for j in range(3):
+        print('Number is too high.')
+
+#assignment2
+total=0
+for i in range(5):
+    n=int(input('Put a number:'))
+    ans=input("Do you want that number included? (y)es or (n)o:")
+    if ans=='y':
+        total+=n
+print('Total result:',total)
+
+#assignment3
 up_d=input("Choose a direction 'Up' or 'Down' :")
 if up_d=='Up':
     num=int(input('Maximum number: '))
@@ -10,11 +30,13 @@ elif up_d=='Down':
 else:
     print('I don\'t understand.')
 
-amount = int(input('How many people do you want to invite?'))
-if amount < 10:
+#assignment4
+amount=int(input('How many people do you want to invite?'))
+if amount<10 and amount>0:
     for i in range(amount):
-        name = input("What is the person\'s name?")
+        name=input('What is the person\'s name?')
         print(f'{name} has been invited.')
-
-elif amount >= 10:
-    print("Too many people.")
+elif amount>=10:
+    print('Too many people.')
+else:
+    print('Stop this nonsense!')
